668a88e917516e931e977b42f050fdd0296c6c9c
alikia2x <alikia2x@outlook.com>
2024-09-22 20:39:49 +0800

    ref: use bun tester

--- test/validLink.test.ts
+++ test/validLink.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test } from "@jest/globals";
+import { describe, expect, test } from "bun:test";
 import validLink, { validTLD } from "../lib/url/validLink";
 
 describe("Check if a string is an accessible domain/URL/IP", () => {
